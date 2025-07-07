1d1795a3100a931a1d1672bf2bd3d548e1eb894b
Alexander Kurz <alex.kurz95@gmail.com>
2025-07-07 10:04:56 +0200

    Print backbone model summary

--- src/backbones.py
+++ src/backbones.py
@@ -9,13 +9,13 @@ class ConvNet(nn.Module):
     Adapted from: https://docs.pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
     """
 
-    def __init__(self):
+    def __init__(self, n_features: int):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2)  # 28*28->32*32-->28*28  # fmt: skip
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1)  # 10*10
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 3)
+        self.fc2 = nn.Linear(120, n_features)
         # self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
@@ -26,3 +26,10 @@ class ConvNet(nn.Module):
         x = F.relu(self.fc2(x))
         # x = self.fc3(x)
         return x
+
+
+if __name__ == "__main__":
+    from torchinfo import summary
+
+    backbone = ConvNet(n_features=3)
+    summary(backbone, input_size=(1, 1, 28, 28))
--- src/headers.py
+++ src/headers.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 
 
 class LinearHeader(nn.Module):
-    def __init__(self, in_features: int, n_classes: int):
+    def __init__(self, n_features: int, n_classes: int):
         super().__init__()
 
-        self.fc = nn.Linear(in_features, n_classes)
+        self.fc = nn.Linear(n_features, n_classes)
 
     def forward(self, x):
         x = self.fc(x)
